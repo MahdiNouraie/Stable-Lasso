828d84cd5cd34717f0852f48330c8b9cc268c447
Mahdi Nouraie <93427794+MahdiNouraie@users.noreply.github.com>
2026-07-16 11:51:42 +1000

    Update Comparison_Func.R

--- Figure6ab/Comparison_Func.R
+++ Figure6ab/Comparison_Func.R
@@ -1,4 +1,4 @@
-# This script generates comparison of differenrt functions
+# This script generates comparison of differenrt functional forms of w
 options(warn=-1) # Turn off warnings
 if (!requireNamespace("MASS")) {install.packages("MASS")} # install package if not already installed
 if (!requireNamespace("glmnet")) {install.packages("glmnet")} # install package if not already installed
